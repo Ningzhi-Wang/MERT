fix: implement logistic log pdf with float32 for better numerical stability

mert_fairseq/models/mert/utils.py
@@ -22,8 +22,8 @@ def discretized_mix_logistic_loss(
         Tensor loss
     """
     # [Batch_size, width, height, channel]
-    pred = pred.permute(0, 2, 1)
-    data = data.permute(0, 2, 1)
+    pred = pred.permute(0, 2, 1).to(torch.float32)
+    data = data.permute(0, 2, 1).to(torch.float32)
     # Number of logistic distributions
     nr_mix = pred.shape[-1] // 3
     # unpack paramteres: distribution probability, mean, log scale
@@ -49,9 +49,9 @@ def discretized_mix_logistic_loss(
     mid_in = inv_stdv * centered_data
     log_pdf_mid = mid_in - log_scales - 2.0 * F.softplus(mid_in)
 
-    inner_inner_cond = (cdf_delta > 1e-4).float()
+    inner_inner_cond = (cdf_delta > 1e-5).float()
     inner_inner_out = inner_inner_cond * torch.log(
-        torch.clamp(cdf_delta, min=1e-4)
+        torch.clamp(cdf_delta, min=1e-12)
     ) + (1.0 - inner_inner_cond) * (
         log_pdf_mid - torch.log(torch.tensor((num_classes - 1) / 2))
     )
@@ -64,7 +64,7 @@ def discretized_mix_logistic_loss(
     log_probs = log_probs + F.log_softmax(logit_probs, dim=-1)
 
     losses = -torch.mean(torch.logsumexp(log_probs, dim=-1), dim=1)
-    return losses.mean()
+    return losses.mean().to(torch.float16)
 
 
 class Scaler(nn.Module):
@@ -109,11 +109,9 @@ def mix_logistic_loss(x, y):
     nr_mix = x.shape[-1] // 3
     logit_probs = x[..., :nr_mix]
     mean = x[..., nr_mix : 2 * nr_mix]
-    scales = F.softplus(x[..., 2 * nr_mix :])
-    log_probs = F.log_softmax(logit_probs, dim=-1)
-    mix = dist.Categorical(logits=log_probs)
-    comp = logistic_dist(mean, scales)
-    mixture_distribution = dist.MixtureSameFamily(mix, comp)
-    loss = -mixture_distribution.log_prob(y)
-    return loss.mean()
-
+    log_scales = x[..., 2 * nr_mix : 3 * nr_mix].to(torch.float32)
+    z = (y - mean).to(torch.float32) / torch.exp(log_scales)
+    mix_probs = F.log_softmax(logit_probs, dim=-1)
+    log_probs = -log_scales - 2. * F.softplus(-z)
+    loss = -torch.logsumexp(log_probs + mix_probs, dim=-1).mean(dim=1)
+    return loss.mean().to(torch.float16)